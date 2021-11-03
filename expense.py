9d6b12231bcbd9f540896315c75eecbf2e4f2435
jordanivan <jordanivan@hotmail.fr>
2021-11-03 19:42:23 +0100

    Expense registry is stored in a csv file

--- expense.py
+++ expense.py
@@ -1,4 +1,7 @@
 from PyInquirer import prompt
+from csv import DictWriter
+
+headersCSV = ['amount','label','spender']      
 
 expense_questions = [
     {
@@ -23,6 +26,12 @@ expense_questions = [
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+
+    print(infos)
+    with open('expense.csv', 'a', newline='') as f_object:
+            dictwriter_object = DictWriter(f_object, fieldnames=headersCSV)
+            dictwriter_object.writerow(infos)            
+            f_object.close()
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
