9d20ecaf81ba419230eb15e857700a82196da37c
jordanivan <jordanivan@hotmail.fr>
2021-11-03 20:12:54 +0100

    A new user can be created

--- user.py
+++ user.py
@@ -1,8 +1,16 @@
 from PyInquirer import prompt
-user_questions = [
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"Name",
+        "message":"New User - Name",
+    },
 ]
 
 def add_user():
+    infos = prompt(user_questions)
     # This function should create a new user, asking for its name
-    return
+    print(infos)
+    print("User added !")
+    return True
